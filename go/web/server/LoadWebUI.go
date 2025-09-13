aed1c8f5cff833bb14f1c277983f84472bfd9ac0
Sharon Aicler <saichler@gmail.com>
2025-09-12 19:03:35 -0700

    refresh web dir

--- go/web/server/LoadWebUI.go
+++ go/web/server/LoadWebUI.go
@@ -10,12 +10,38 @@ import (
 
 func (this *RestServer) LoadWebUI() {
 	fmt.Println("Loading UI...")
-	fs := http.FileServer(http.Dir("web"))
-	this.loadWebDir("/", fs)
+	
+	// Create a new ServeMux to clear previous handlers
+	http.DefaultServeMux = http.NewServeMux()
+	
+	// Determine the web directory path
+	webDir := this.getWebDirectory()
+	
+	fs := http.FileServer(http.Dir(webDir))
+	this.loadWebDir("/", fs, webDir)
 }
 
-func (this *RestServer) loadWebDir(path string, fs http.Handler) {
-	dirName := concat("./web", path)
+func (this *RestServer) getWebDirectory() string {
+	// Try to find web directory in various locations
+	possiblePaths := []string{
+		"web",           // Current directory
+		"./web",         // Relative to current
+		"../web",        // Up one level
+		"../../web",     // Up two levels
+	}
+	
+	for _, path := range possiblePaths {
+		if _, err := os.Stat(path); err == nil {
+			return path
+		}
+	}
+	
+	// Default to "web" if not found
+	return "web"
+}
+
+func (this *RestServer) loadWebDir(path string, fs http.Handler, webDir string) {
+	dirName := concat(webDir, path)
 	files, err := os.ReadDir(dirName)
 	if err != nil {
 		fmt.Println("Error loading web UI:", err)
@@ -25,12 +51,12 @@ func (this *RestServer) loadWebDir(path string, fs http.Handler) {
 		//filePath := app("./web", path, file.Name())
 		webPath := concat(path, file.Name())
 		if file.IsDir() {
-			this.loadWebDir(concat(webPath, "/"), fs)
+			this.loadWebDir(concat(webPath, "/"), fs, webDir)
 		} else {
 			if file.Name() == "index.html" {
 				fmt.Println("Loaded index.html")
 				http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-					http.ServeFile(w, r, filepath.Join("web", "index.html"))
+					http.ServeFile(w, r, filepath.Join(webDir, "index.html"))
 				})
 			} else {
 				fmt.Println("Loaded file:", webPath)
