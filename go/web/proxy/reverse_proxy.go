3fe5f1369c80630a04dbaf584aa79642e5145279
Sharon Aicler <saichler@gmail.com>
2025-12-07 05:53:56 -0800

    add 24444

--- go/web/proxy/reverse_proxy.go
+++ go/web/proxy/reverse_proxy.go
@@ -65,22 +65,22 @@ func NewReverseProxy() *ProxyConfig {
 				},
 			},
 			{
-				ListenPort: ":24443",
+				ListenPort: ":9092",
 				Routes: []RouteConfig{
 					{
 						Domains:    []string{"www.probler.dev", "probler.dev"},
-						TargetPort: "23443",
+						TargetPort: "9093",
 						CertFile:   "probler.dev/domain.cert.pem",
 						KeyFile:    "probler.dev/private.key.pem",
 					},
 				},
 			},
 			{
-				ListenPort: ":9092",
+				ListenPort: ":24443",
 				Routes: []RouteConfig{
 					{
 						Domains:    []string{"www.probler.dev", "probler.dev"},
-						TargetPort: "9093",
+						TargetPort: "23443",
 						CertFile:   "probler.dev/domain.cert.pem",
 						KeyFile:    "probler.dev/private.key.pem",
 					},
